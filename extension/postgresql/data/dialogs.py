bb17b821bca115c73da447107963d5d98c675f7b
magomedovr89 <magomedovr89@gmail.com>
2022-09-04 00:15:59 +0300

    test_used_git_on_CMD

--- extension/postgresql/data/dialogs.py
+++ extension/postgresql/data/dialogs.py
@@ -4,25 +4,9 @@ from aiogram import Bot, Dispatcher, executor, types
 
 message = {
     'ru': {
-        'greeting': f'Приветствую тебя пользователь. Ты запустил бота для сбора информации.',
+        'greeting': f'Приветствую тебя пользователь. \nТы запустил бота для сбора информации.',
         'help_information': 'text',
         'work_information': 'text',
         'reference_work': 'text'
     }
 }
-
-
-
-'''
-@dp.message_handler(commands="test4")
-async def with_hidden_link(message: types.Message):
-    await message.answer(
-        f"{fmt.hide_link('https://telegram.org/blog/video-calls/ru')}Кто бы мог подумать, что "
-        f"в 2020 году в Telegram появятся видеозвонки!\n\nОбычные голосовые вызовы "
-        f"возникли в Telegram лишь в 2017, заметно позже своих конкурентов. А спустя три года, "
-        f"когда огромное количество людей на планете приучились работать из дома из-за эпидемии "
-        f"коронавируса, команда Павла Дурова не растерялась и сделала качественные "
-        f"видеозвонки на WebRTC!\n\nP.S. а ещё ходят слухи про демонстрацию своего экрана :)",
-        parse_mode=types.ParseMode.HTML)
-    
-'''
